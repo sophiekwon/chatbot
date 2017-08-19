d4f4f1010db44f729ddc4898fc3168b4bb379d2f
Kwon, Seohee | Sophie | COED <seohee.kwon@rakuten.com>
2017-08-19 21:14:30 +0900

    changed way

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, I am a chat bot')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
 
 app.get('/facebook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
